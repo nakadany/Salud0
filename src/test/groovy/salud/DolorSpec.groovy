3afdd97fcf3fc1f9b8cc34c3ef78acdb3fe90fdd
nakadany <daniel_r_b@hotmail.com>
2018-12-19 12:06:24 +0100

    Merge branch 'crear_nueva_clase_tema' into dev
    # Conflicts:
    #	grails-app/domain/salud/Tema.groovy

---
+++ src/test/groovy/salud/DolorSpec.groovy
@@ -0,0 +1,18 @@
+package salud
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class DolorSpec extends Specification implements DomainUnitTest<Dolor> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
